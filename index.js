9677265b6642fc59a68f1f87fe5fc341ddd4ddfe
Jeremy Nelson <jeremy.nelson8@gmail.com>
2022-10-14 11:12:03 -0400

    wildcard/404 route

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ require('dotenv').config()
 // Declare app variable and assign it to express
 const app = express()
 
+// Declares port variable, assigns it value from .env file 
+port = process.env.PORT
+
 // Home route. This determines the behavior of the URL's default route.
 app.get('/', (req, res) => {
     res.send('Hello, Welcome to an express server')
 })
 
-// Declares port variable, assigns it value from .env file 
-port = process.env.PORT
+// Wildcard route will allow us to show a 404/error page.
+app.get('*', (req, res)=>{
+    res.status(404).send(`<h1>404 Page</h1>`)
+})
+
 
 // Calls listen method to turn on server and report back if it is running.
 app.listen(port, (req, res) =>{
